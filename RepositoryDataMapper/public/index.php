da104ab931d21e1e9878bc1045a805aba4c8f5ad
NiShiGara <91442220+NiShiGara@users.noreply.github.com>
2022-06-12 00:09:08 +1000

    Update index.php

--- RepositoryDataMapper/public/index.php
+++ RepositoryDataMapper/public/index.php
@@ -2,7 +2,6 @@
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-
 use App\Controllers\Controller;
 use App\Model\User;
 use App\Repository\UserRepository;
@@ -13,30 +12,27 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $loader = new FilesystemLoader(dirname(__DIR__) . '/templates/');
 $twig = new Environment($loader);
 $connection = new Controller($twig);
-//$user = new User();
 $reposy = new UserRepository();
-$connection->__invoke($reposy->RepGetAll());
+$connection->__invoke($reposy->RGetAll());
 
 
 $action = $_POST['action'];
-
-if ($_POST['id'] != null && $_POST['nickname'] != null && $_POST['name'] != null && $_POST['surname'] != null && $_POST['age'] != null) {
+if ($_POST['id'] != null && $_POST['nickname'] != null && $_POST['name'] != null && $_POST['surname'] != null && $_POST['age'] != null)
+{
     $user_id = (int)$_POST['id'];
     $user_nickname = (string)$_POST['nickname'];
     $user_name = (string)$_POST['name'];
     $user_surname = (string)$_POST['surname'];
     $user_age = (int)$_POST['age'];
 
-    $u = new User();
-    $u->setall($user_id, $user_nickname, $user_name, $user_surname, $user_age);
-
-    switch ($action) {
+    switch ($action)
+    {
         case "add":
-            $reposy->Radd($u);
+            $reposy->Radd($user_id, $user_nickname, $user_name, $user_surname, $user_age);
             break;
-//        case "upd":
-//            $u->update();
-//            break;
+        case "upd":
+            $reposy->Rupdate($user_id, $user_nickname, $user_name, $user_surname, $user_age);
+            break;
 
     }
     header('Refresh: 0;');
@@ -44,8 +40,10 @@ if ($_POST['id'] != null && $_POST['nickname'] != null && $_POST['name'] != null
 
 $action2 = $_POST['action2'];
 $user_id2 = (int)$_POST['id2'];
-if ($user_id2 != null) {
-    switch ($action2) {
+if ($user_id2 != null)
+{
+    switch ($action2)
+    {
         case "find":
             $reposy->findById($user_id2);
             break;
@@ -59,8 +57,8 @@ if ($user_id2 != null) {
 $connection->__FindFormInvoke();
 $action3 = $_POST['action3'];
 $value = $_POST['value'];
-if ($value != null) {
+if ($value != null)
+{
     $u2 = new User();
     $connection->__FindInvoke($reposy->RfindByValue("$action3", $value), $value);;
 }
-
